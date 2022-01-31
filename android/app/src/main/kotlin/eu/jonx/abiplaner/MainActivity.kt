ec2256f3bb1296f9f1e8344b3f278fcdff180c6f
Jonx <dev.jonx.eu@gmail.com>
2022-01-31 01:25:52 +0100

    basic files structure

---
+++ android/app/src/main/kotlin/eu/jonx/abiplaner/MainActivity.kt
@@ -0,0 +1,6 @@
+package eu.jonx.abiplaner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Abiplaner',
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: const MyHomePage(title: 'MainPage'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[Text('Hallo Welt')],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: Test,
+        tooltip: 'Toast',
+        child: const Icon(Icons.slideshow),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void Test(){
+    Fluttertoast.showToast(
+        msg: "This is a test",
+        toastLength: Toast.LENGTH_SHORT,
+    );
+  }
+
+}
