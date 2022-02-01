a7469da115fa0c414c79b8f647fd2b891a46b580
Jonx <dev.jonx.eu@gmail.com>
2022-02-01 15:52:50 +0100

    changed Theme1

--- lib/data/Themes.dart
+++ lib/data/Themes.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 
 class MyThemes {
   static final Theme1 = ThemeData(
-    backgroundColor: Colors.blue.shade800,
-    scaffoldBackgroundColor: const Color(0xFF192131),
-    colorScheme: const ColorScheme.dark(),
-    canvasColor: Colors.lightBlue,
-
+    backgroundColor: const Color(0xFF182437),
+    scaffoldBackgroundColor: const Color(0xFF182437),
+    canvasColor: Colors.lightBlue, colorScheme: const ColorScheme.dark().copyWith(secondary: const Color(0xFF6490d6)),
+    //Listview
   );
   static final Theme2 = ThemeData(
 
   );
+
+  var pallette = <Color>[
+    const Color(0xFF182437), // Background
+    const Color(0xFF6490d6), // Accent
+    const Color(0xFF283C5C), // Tabbar
+
+  ];
 }
