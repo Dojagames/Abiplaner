2ad7d376303627857a9af7c98c396c0c592ab060
sturmkarotte <jonas-etzold@web.de>
2022-02-03 20:27:28 +0100

    improved menu bar

--- lib/tabs/Home.dart
+++ lib/tabs/Home.dart
@@ -6,24 +6,26 @@ import 'package:abiplaner/data/Themes.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
+final GlobalKey<ScaffoldState> _menuOpen = GlobalKey();
 
 Widget HomeWidget =
 Scaffold(
   drawer: const MenuBar(),
+  drawerEnableOpenDragGesture: false,
+  key: _menuOpen,
   appBar: AppBar(
     title: Text("Welcome $username"),
     centerTitle: true,
     backgroundColor: MyThemes.ThemeMain.bottomAppBarColor,
-    actions: [
+    leading:
       IconButton(icon: const Icon(Icons.settings), enableFeedback: false, splashRadius: 5, onPressed: () {
-
+        _menuOpen.currentState!.openDrawer();
         Fluttertoast.showToast(
             msg: 'opening settings menu',
             toastLength: Toast.LENGTH_SHORT,
         );
         //Settingsmenu
       }),
-    ],
   ),
   body: Container(
     child: SfCalendar(
