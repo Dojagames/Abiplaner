1104f03149ce3016c24f02902f35ecde6c107145
Jonx <dev.jonx.eu@gmail.com>
2022-02-03 20:34:20 +0100

    moved calendar

--- lib/tabs/Calendar.dart
+++ lib/tabs/Calendar.dart
@@ -1,3 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-Widget CalenderWidget = Center(child: Text("Page 3"));
+Widget CalenderWidget =
+  Scaffold(
+    body: Container(
+      child: SfCalendar(
+        view: CalendarView.month,
+      ),
+    )
+);
