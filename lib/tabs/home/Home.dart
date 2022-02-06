c243217ef5c3c16f734d4bd5eab5548cc501092a
Jonx <dev.jonx.eu@gmail.com>
2022-02-06 14:15:07 +0100

    Fixed Timetable / destroyed clean code

--- lib/tabs/home/Home.dart
+++ lib/tabs/home/Home.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:abiplaner/tabs/home/menus/MenuBar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -5,7 +6,7 @@ import 'package:abiplaner/data/vars.dart';
 import 'package:abiplaner/data/Themes.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-
+var physicalScreenSize = window.physicalSize;
 final GlobalKey<ScaffoldState> _menuOpen = GlobalKey();
 
 Widget HomeWidget =
@@ -28,77 +29,86 @@ Scaffold(
       }),
   ),
   body:
-        GridView.count(
-          crossAxisCount: 5,
-          crossAxisSpacing: 10.0,
-          mainAxisSpacing: 8.0,
+    Column(
+      children: [
+        SizedBox(height: 5),
+        SizedBox(height: 20,width:physicalScreenSize.width, child: Row(children: [SizedBox(width: 40), Expanded(child: GridView.count(crossAxisCount: 5, crossAxisSpacing: 10,children: const [Text("Mon",textAlign: TextAlign.center),Text("Tue",textAlign: TextAlign.center),Text("Wed",textAlign: TextAlign.center), Text("Thu",textAlign: TextAlign.center), Text("Fri",textAlign: TextAlign.center)],),), SizedBox(width: 10,),],),),
+        SizedBox(width:physicalScreenSize.width, height: 500,child:Row(
           children: [
-            Center(child: Text("Mon", textAlign: TextAlign.center,)),
-            Center(child: Text("Tue", textAlign: TextAlign.center,)),
-            Center(child: Text("Wed", textAlign: TextAlign.center,)),
-            Center(child: Text("Thu", textAlign: TextAlign.center,)),
-            Center(child: Text("Fri", textAlign: TextAlign.center,)),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color(0xFF2f3a4b),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color(0xFF2f3a4b),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color(0xFF2f3a4b),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color(0xFF2f3a4b),
-              ),
-            ),Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color(0xFF2f3a4b),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color(0xFF2f3a4b),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color(0xFF2f3a4b),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color(0xFF2f3a4b),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color(0xFF2f3a4b),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color(0xFF2f3a4b),
-              ),
-            ),
+            SizedBox(width: 40),
+            Expanded(child: GridView.count(
+              crossAxisCount: 5,
+                crossAxisSpacing: 10.0,
+                mainAxisSpacing: 8.0,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xFF2f3a4b),
+                    ),
+                  ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF2f3a4b),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF2f3a4b),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF2f3a4b),
+                      ),
+                    ),Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF2f3a4b),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF2f3a4b),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF2f3a4b),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF2f3a4b),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF2f3a4b),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF2f3a4b),
+                      ),
+                    ),
 
 
-          ],
-  ),
+                  ],
+             ),
+        ),
+        SizedBox(width: 10),
+              ],
+
+            ),),
+      ],
+    ),
 );
