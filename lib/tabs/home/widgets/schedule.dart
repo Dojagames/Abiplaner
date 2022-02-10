1f1800415f51cbeb2eca72f30ed5971e1b296fe9
Jonx <dev.jonx.eu@gmail.com>
2022-02-10 08:02:17 +0100

    ultra improvement (changed grid to gridbuilder)

--- lib/tabs/home/widgets/schedule.dart
+++ lib/tabs/home/widgets/schedule.dart
@@ -1,5 +1,10 @@
+import 'dart:ffi';
+
+import 'package:abiplaner/data/vars.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 String _currentSubject = "";
 String _number = "";
@@ -13,56 +18,27 @@ setCurSub(String _sub) {
   _currentSubject = _sub;
 }
 
+String getText(int _number){
+  int _lessonInt = (_number/5).floor();
+  for(int i = 0; i < 5; i++){
+    if((_number - i) % 5 == 0) return subjectSchedule[i][_lessonInt];
+  }
+return "";
+}
 
 
-Widget BoxWidget =
-  Container(
+Color getColor(){
+  return Colors.green;
+}
+
+
+Widget boxWidget(String _number) => Container(
   decoration: BoxDecoration(
     borderRadius: BorderRadius.circular(10),
     color: Color(0xFF2f3a4b),
-    ),
-    child: Text(_currentSubject),
-  );
-
-
-
-
-
-Widget NumberWidget = new
-  Container(child: _numbWidget(),);
-
-class _numbWidget extends StatefulWidget {
-  const _numbWidget({Key? key}) : super(key: key);
-
-   @override
-   _numbWidgetState createState() => _numbWidgetState();
-
-
-}
-
-class _numbWidgetState extends State<_numbWidget> {
-  int _count = 0;
-
-  incCount(){
-    _count += 1;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    incCount();
-    return new SizedBox(child: Container(child: Align(alignment: Alignment(0, 0), child: new Text(_count.toString())), height: 34));
-  }
-}
-
-      //   SizedBox(child:
-      //     Container(child:
-      //       Align(alignment:
-      //         Alignment(0, 0), child:
-      //           Text( _count.toString())
-      //       ),
-      //       height: _heightIndex[_count - 1]
-      //   ),
-      // );
+  ),
+  child: Center(child: Text(getText(int.parse(_number)), style: TextStyle(fontSize: 16, color: getColor()),),),
+);
 
 
 
